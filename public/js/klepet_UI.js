bb150501f64461ca30443a3abb04644c39cfe7a0
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2016-04-09 12:17:54 +0000

    Zaključek dela na IV. nalogi

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -78,6 +78,15 @@ $(document).ready(function() {
     $('#sporocila').append(novElement);
   });
   
+  socket.on('dregljaj', function (dregljaj){
+    $('#vsebina').jrumble();
+    $('#vsebina').trigger('startRumble');
+    setTimeout(function(){
+      $('#vsebina').trigger('stopRumble');
+    }, 1500);
+    
+  });
+  
   socket.on('kanali', function(kanali) {
     $('#seznam-kanalov').empty();
 
